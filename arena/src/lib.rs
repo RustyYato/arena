7b65a5ca65bc39ee2fec9c0c6d3fd880f6fa21aa
RustyYato <rustyyato@gmail.com>
2022-05-24 12:49:54 -0400

    add ArenaRef because of of &mut self

--- arena/src/lib.rs
+++ arena/src/lib.rs
@@ -4,3 +4,33 @@ mod local;
 mod slab;
 
 pub use local::Arena;
+
+pub struct ArenaRef<'a, T> {
+    arena: &'a Arena<T>,
+}
+
+impl<'a, T> ArenaRef<'a, T> {
+    pub fn new(arena: &'a mut Arena<T>) -> Self {
+        Self { arena }
+    }
+
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.arena.len()
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.arena.is_empty()
+    }
+
+    #[must_use]
+    pub fn insert(&mut self, value: T) -> &'a mut T {
+        self.arena.insert(value)
+    }
+
+    #[must_use]
+    pub fn insert_all<I: IntoIterator<Item = T>>(&mut self, iter: I) -> &'a mut [T] {
+        unsafe { self.arena.insert_all(iter) }
+    }
+}
